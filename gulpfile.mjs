04d18494d1719cf3ec98ac562d3a74bb838ef1cd
Max Powis <max.powis@gmail.com>
2023-08-08 15:48:16 +0200

    Upgrade package versions and gulpfile as ESM

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,12 +1,15 @@
-var gulp = require('gulp');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var cp = require('child_process');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync');
+import gulp from 'gulp';
+import csso from 'gulp-csso';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import plumber from 'gulp-plumber';
+import cp from 'child_process';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 
 var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
 
